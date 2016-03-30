df42e4a5ad41d771562f73e5b5f506d7b71be21a
Shouko <h999342@gmail.com>
2016-03-31 01:54:57 +0800

    put actions and responses constant to seperate file

---
+++ actions.js
@@ -0,0 +1,19 @@
+module.exports = {
+  welcome: 0,
+  welcome_navigate: 90,
+  add_route: 1,
+  add_station: 11,
+  add_time: 12,
+  add_interval: 13,
+  add_proceed: 91,
+  query: 2,
+  modify: 3,
+  modify_navigate: 30,
+  modify_station: 31,
+  modify_time: 32,
+  modify_interval: 33,
+  modify_proceed: 93,
+  delete: 4,
+  delete_proceed: 94,
+  ask_param: 100
+};
--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 var config = require('./config');
+var actions = require('./actions');
+var responses = require('./responses');
 var request = require('request');
 var Promise = require('bluebird');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(config.db.url);
-var actions = {
-  welcome: 0,
-  welcome_navigate: 90,
-  add_route: 1,
-  add_station: 11,
-  add_time: 12,
-  add_interval: 13,
-  add_proceed: 91,
-  query: 2,
-  modify: 3,
-  modify_navigate: 30,
-  modify_station: 31,
-  modify_time: 32,
-  modify_interval: 33,
-  modify_proceed: 93,
-  delete: 4,
-  delete_proceed: 94,
-  ask_param: 100
-};
 var modify_menu = {
   1: 31,
   2: 32,
   3: 33
-};
-var response_text = {
-  ask_route: "請輸入欲訂閱之公車路號（如：650）",
-  ask_station: "請輸入目標公車站名：",
-  ask_time: "請輸入通知時間區間：\n（如：0810-0840）",
-  ask_interval: "請輸入通知時間間隔：\n（如：5）",
-  ask_delete: "請輸入欲刪除之項目編號：",
-  succeed_add: "您的訂閱已完成！",
-  succeed_modify: "您的訂閱已完成更新！"
 };
 
 var in_queue = new Array();
@@ -105,15 +79,6 @@ function schedule_fetch(wait) {
 
 schedule_fetch(0);
 
-var msg = {
-  main_menu: "歡迎使用臺北市政府 LINE 訂閱公車到站資訊服務\n"
-}
-
-msg.main_menu += ["訂閱公車資訊", "查詢訂閱紀錄", "更改訂閱紀錄", "刪除訂閱紀錄"].map(function(element, index, array) {
-  var option_num = index + 1;
-  return element + ":請輸入 " + option_num;
-}).join("\n");
-
 function Member(mid) {
   this.mid = mid;
   this.params = [];
@@ -176,7 +141,7 @@ Member.prototype.run = function() {
     switch(this.next) {
       case actions.welcome: {
         in_queue[this.mid].shift();
-        this.puts(msg.main_menu);
+        this.puts(responses.main_menu);
         this.set_next_ra(actions.welcome_navigate);
         this.set_next(actions.ask_param);
         break;
@@ -188,32 +153,32 @@ Member.prototype.run = function() {
         break;
       }
       case actions.add_route: {
-        this.puts(response_text.ask_route);
+        this.puts(responses.ask_route);
         this.set_next_ra(actions.add_station);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.add_station: {
-        this.puts(response_text.ask_station);
+        this.puts(responses.ask_station);
         this.set_next_ra(actions.add_time);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.add_time: {
-        this.puts(response_text.ask_time);
+        this.puts(responses.ask_time);
         this.set_next_ra(actions.add_interval);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.add_interval: {
-        this.puts(response_text.ask_interval);
+        this.puts(responses.ask_interval);
         this.set_next_ra(actions.add_proceed);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.add_proceed: {
         // proceed add action with params
-        this.puts(response_text.succeed_add);
+        this.puts(responses.succeed_add);
         this.params = [];
         break;
       }
@@ -224,7 +189,7 @@ Member.prototype.run = function() {
       }
       case actions.modify: {
         this.query();
-        this.puts("請輸入欲更改內容之項目編號：\n到站名稱：請輸入 1\n通知訊息時間：請輸入 2\n通知訊息間隔：請輸入 3");
+        this.puts(responses.ask_modify);
         this.set_next_ra(actions.modify_navigate);
         this.set_next(actions.ask_param);
         break;
@@ -235,39 +200,39 @@ Member.prototype.run = function() {
         break;
       }
       case actions.modify_station: {
-        this.puts(response_text.ask_station)
+        this.puts(responses.ask_station)
         this.set_next_ra(actions.modify_proceed);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.modify_time: {
-        this.puts(response_text.ask_time)
+        this.puts(responses.ask_time)
         this.set_next_ra(actions.modify_proceed);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.modify_interval: {
-        this.puts(response_text.ask_interval)
+        this.puts(responses.ask_interval)
         this.set_next_ra(actions.modify_proceed);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.modify_proceed: {
         // do dome modify job with params[2]
-        this.puts(response_text.succeed_modify);
+        this.puts(responses.succeed_modify);
         this.params = [];
         break;
       }
       case actions.delete: {
         this.query();
-        this.puts(response_text.ask_delete);
+        this.puts(responses.ask_delete);
         this.set_next_ra(actions.delete_proceed);
         this.set_next(actions.ask_param);
         break;
       }
       case actions.delete_proceed: {
         // do dome delete job with params[1]
-        this.puts(response_text.succeed_modify);
+        this.puts(responses.succeed_modify);
         this.params = [];
         break;
       }

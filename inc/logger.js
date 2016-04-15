edd993fc8dec956175b60b2020248b56bfaf718d
shouko <imshouko@gmail.com>
2016-04-15 17:15:04 +0800

    fix syntax

--- inc/logger.js
+++ inc/logger.js
@@ -3,7 +3,7 @@ var winston = require('winston');
 module.exports = function(filename) {
   return new (winston.Logger)({
     transports: [
-      new (winston.transports.Console)( timestamp: true, level: 'info' ),
+      new (winston.transports.Console)({ timestamp: true, level: 'info'}),
       new (winston.transports.File)({ timestamp: true, filename: filename, level: 'error' })
     ]
   });
